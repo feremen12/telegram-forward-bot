b3708b29929e3cb02f1b08ad9db2cb9ef0bf6e99
feremen12 <tester.method83@gmail.com>
2026-06-07 21:55:54 +0330

    Update forward_bot.py

--- forward_bot.py
+++ forward_bot.py
@@ -10,7 +10,7 @@ API_HASH = os.environ.get("API_HASH", "")
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
 
 # کانال مبدا (که می‌خوای از اون بخونی)
-SOURCE_CHANNEL =  "@testforwarder83"  # مثلاً @bbcpersian
+SOURCE_CHANNEL =  ["@testforwarder83" , "@alonews"]  # مثلاً @bbcpersian
 
 # کانال مقصد (کانال خودت)
 DEST_CHANNEL = -1003942314547  # مثلاً @my_channel
